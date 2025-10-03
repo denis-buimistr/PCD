4036e299665fc1a405dc0da4f098955817fa4b6c
Buimistr Denis <denis.buimistr@gmail.com>
2025-10-03 16:14:01 +0300

    Условие 1 - done

---
+++ src/Variant1.java
@@ -0,0 +1,36 @@
+public class Variant1 extends Thread{  // суммирование с первого элемента 
+      private int[] mas;
+      private int sum;
+
+ public Variant1(int[] array) {
+    this.mas = mas;
+    this.sum = 0;
+    }  
+
+        @Override
+    public void run() {
+        System.out.println("\n" + getName() + "Поиск и суммирование с первого элемента" );
+        System.out.println("=".repeat(70));
+
+        for(int i = 0; i < mas.length; i ++) {
+            if (mas[i] % 2 == 0) { // проверяю на чётность
+                sum += mas[i];
+                System.out.println();
+            }
+
+            try {
+                Thread.sleep(50); // сделал небольшую задержку для наглядности
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("=".repeat(70));
+        System.out.println(getName() + "Сумма чётных: " + sum);
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+}
+
