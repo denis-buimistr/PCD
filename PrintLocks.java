1b5af1f41e74bb934fa00601ba0001e54f744286
Buimistr Denis <denis.buimistr@gmail.com>
2025-11-03 23:13:29 +0200

    lab_4

---
+++ PrintLocks.java
@@ -0,0 +1,6 @@
+// PrintLocks.java
+public class PrintLocks {
+    // Доступны из любого файла
+    public static final Object LOCK_12 = new Object();
+    public static final Object LOCK_34 = new Object();
+}
